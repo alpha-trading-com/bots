d1b6c2aab3af52812b610ff9428525bc7b200fd7
pycorn <hayesdominique0729@gmail.com>
2025-05-12 16:00:47 +0000

    feat: implement unstakign if key stroke

---
+++ unstake_if_key_stroke.py
@@ -0,0 +1,36 @@
+import time
+import bittensor as bt
+from utils.const import sn_vali_addr
+from utils.utils import *
+
+        
+if __name__ == '__main__':
+    
+    netuid = int(input("Enter the netuid: "))
+
+    subtensor = bt.subtensor('finney')
+    subnet = subtensor.subnet(netuid=netuid)
+    alpha_price = subnet.alpha_to_tao(1)
+    print(f"Current alpha token price: {alpha_price} TAO")
+
+    wallet_name = input("Enter the wallet name: ")            
+    wallet = bt.wallet(name=wallet_name)
+    wallet.unlock_coldkey()
+    round_table_hotkey = '5FFApaS75bv5pJHfAp2FVLBj9ZaXuFDjEypsaBNc1wCfe52v'
+    dest_hotkey = input("Enter the destination hotkey (default is Round table): ") or round_table_hotkey
+    
+    print("Press 'y' to unstake, or Ctrl+C to exit")
+    try:
+        if input().lower() == 'y':
+            while True:
+                try:
+                    unstake_from_subnet(netuid, subtensor, wallet, dest_hotkey)
+                except Exception as e:
+                    logger.error(f"Error: {e}")
+                    continue
+                break
+    except KeyboardInterrupt:
+        print("\nExiting...")
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        
