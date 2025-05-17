32160420328cf2fe1c5956d51fa805227bd58f0e
pycorn <hayesdominique0729@gmail.com>
2025-05-17 17:16:39 +0000

    feat: add quick stake

---
+++ quick_stake.py
@@ -0,0 +1,37 @@
+import time
+import bittensor as bt
+from utils.const import sn_vali_addr
+from utils.utils import *
+
+        
+if __name__ == '__main__':
+    
+    netuid = int(input("Enter the netuid: "))
+
+    subtensor = bt.subtensor('finney')
+    subnet = subtensor.subnet(netuid=netuid)
+    alpha_price = subnet.alpha_to_tao(1)
+    print(f"Current alpha token price: {alpha_price} TAO")
+
+    wallet_name = input("Enter the wallet name: ") or 'stake_2'           
+    wallet = bt.wallet(name=wallet_name)
+    wallet.unlock_coldkey()
+
+    round_table_hotkey = '5FFApaS75bv5pJHfAp2FVLBj9ZaXuFDjEypsaBNc1wCfe52v'
+    dest_hotkey = input("Enter the destination hotkey (default is Round table): ") or round_table_hotkey
+    
+    print("Press 'y' to stake, or Ctrl+C to exit")
+    try:
+        while True:
+            try:
+                result = stake_to_subnet(netuid, subtensor, wallet, dest_hotkey)
+                if result:
+                    break
+            except Exception as e:
+                logger.error(f"Error: {e}")
+                continue
+    except KeyboardInterrupt:
+        print("\nExiting...")
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        
