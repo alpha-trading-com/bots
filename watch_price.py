1b13b753d44b555364923908855e3fb8981101d4
larry-neil-1206 <larryneilmcgowen1206@gmail.com>
2025-05-27 10:01:32 -0500

    chore: use archive node

--- watch_price.py
+++ watch_price.py
@@ -1,8 +1,10 @@
 import bittensor as bt
 from utils.utils import *
+from constants import NETWORK
+
 
 if __name__ == '__main__':
     netuid = int(input("Enter the netuid: "))
-    subtensor = bt.subtensor('finney')
+    subtensor = bt.subtensor(network=NETWORK)
     
     watching_price(netuid, subtensor)
