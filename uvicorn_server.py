fdfba060ce9437d5d0a587da95b2149aa3eff67e
larry-neil-1206 <larryneilmcgowen1206@gmail.com>
2025-05-31 10:43:55 -0500

    chore: update frontend

--- uvicorn_server.py
+++ uvicorn_server.py
@@ -5,13 +5,19 @@ import subprocess
 from typing import Dict
 from fastapi import Depends
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 from auth import get_current_username
 from constants import ROUND_TABLE_HOTKEY, NETWORK
 
 app = fastapi.FastAPI()
 
-wallet_names = ["stake_2",]
+# Set up templates
+templates = Jinja2Templates(directory="templates")
+
+wallet_names = ["stake_2","sec_ck4",]
 wallets: Dict[str, bt.wallet] = {}
 
 def unlock_wallets():
@@ -22,8 +28,7 @@ def unlock_wallets():
         wallets[wallet_name] = wallet
 
 @app.get("/")
-def read_root(username: str = Depends(get_current_username)):
-    from index import INDEX_HTML
+def read_root(request: fastapi.Request, username: str = Depends(get_current_username)):
     subtensor = bt.subtensor(network=NETWORK)
     def get_balance_html():
         balance_html = ""
@@ -32,15 +37,28 @@ def read_root(username: str = Depends(get_current_username)):
             balance = subtensor.get_balance(wallet.coldkey.ss58_address)
             balance_html += f"""
                 <div class="balance-container">
-                    <div class="balance-title"><a target="_blank" href="/stake_list?wallet={wallet_name}" style="text-decoration: none; color: inherit; cursor: pointer; text-decoration: underline;">{wallet_name}</a></div>
+                    <div class="balance-title"><a target="_blank" href="/stake_list?wallet_name={wallet_name}" style="text-decoration: none; color: inherit; cursor: pointer; text-decoration: underline;">{wallet_name}</a></div>
                     <div class="balance-amount">{balance} TAO</div>
                 </div>
             """
         return balance_html
 
-    return fastapi.responses.HTMLResponse(
-        content=INDEX_HTML.format(balance_html=get_balance_html())
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "balance_html": get_balance_html(), "wallet_names": wallet_names}
     )
+
+
+@app.get("/min_tolerance")
+def min_tolerance(
+    tao_amount: float, 
+    netuid: int, 
+):
+    subtensor = bt.subtensor(network=NETWORK)
+    subnet = subtensor.subnet(netuid=netuid)
+    min_tolerance = tao_amount / subnet.tao_in.tao  
+    return {"min_tolerance": min_tolerance}
+
 
 @app.get("/stake")
 def stake(
@@ -49,21 +67,26 @@ def stake(
     wallet_name: str="stake_2", 
     dest_hotkey: str = ROUND_TABLE_HOTKEY, 
     rate_tolerance: float = 0.005,
+    min_tolerance_staking: bool = True,
+    retries: int = 1,
     username: str = Depends(get_current_username)
 ):
-    retries = 4
+    if retries < 1:
+        retries = 1
+    result = None
+    min_tolerance = None
     while retries > 0:
         try:
             wallet = wallets[wallet_name]
             subtensor = bt.subtensor(network=NETWORK)
             subnet = subtensor.subnet(netuid=netuid)
             min_tolerance = tao_amount / subnet.tao_in.tao  
-            if rate_tolerance < min_tolerance:
-                rate_tolerance = min_tolerance + 0.0001
+            if min_tolerance_staking:
+                rate_tolerance = min_tolerance + 0.001
 
             result = subtensor.add_stake(
                 netuid=netuid,
-                amount= bt.Balance.from_tao(tao_amount),
+                amount= bt.Balance.from_tao(tao_amount, netuid),
                 wallet=wallet,
                 hotkey_ss58=dest_hotkey,
                 safe_staking=True,
@@ -71,12 +94,19 @@ def stake(
             )
             if not result:
                 raise Exception("Stake failed")
-            return {"message": f"Staked {tao_amount} TAO from {wallet_name}, min_tolerance: {min_tolerance}", "result": result}
+            
+            return {
+                "success": True,
+                "result": result,
+                "min_tolerance": min_tolerance,
+            }
         except Exception as e:
             retries -= 1
             if retries == 0:
                 return {
-                    "message": f"Error staking {tao_amount} TAO from {wallet_name}: {e}, min_tolerance: {min_tolerance}", "result": None,
+                    "success": False,
+                    "result": result,
+                    "min_tolerance": min_tolerance,
                 }
 
 
@@ -84,29 +114,45 @@ def stake(
 def unstake(
     netuid: int,
     wallet_name: str="stake_2",
+    amount: float = None,
     dest_hotkey: str = ROUND_TABLE_HOTKEY,
+    retries: int = 1,
     username: str = Depends(get_current_username)
 ):
-    retries = 4
+    if retries < 1:
+        retries = 1
+    result = None
     while retries > 0:
         try:
+
             wallet = wallets[wallet_name]
             subtensor = bt.subtensor(network=NETWORK)
+            if amount is not None:
+                subnet = subtensor.subnet(netuid=netuid)
+                amount = bt.Balance.from_tao(amount / subnet.price.tao, netuid)
+
             result = subtensor.unstake(
                 netuid=netuid, 
                 wallet=wallet, 
+                amount=amount,
                 hotkey_ss58=dest_hotkey,
-                safe_staking=True,
-                rate_tolerance=1.0,
             )
+            print(result)
             if not result:
                 raise Exception("Unstake failed")
             
-            return {"message": f"Unstaked from {netuid} network", "result": result}
+            return {
+                "success": True,
+                "result": result,
+            }
         except Exception as e:
             retries -= 1
             if retries == 0:
-                return {"message": f"Error unstaking from {netuid} network: {e}", "result": False}
+                return {
+                    "success": False,
+                    "result": result,
+                }
+
 
 @app.get("/stake_list")
 def stake_list(wallet_name: str = "stake_2"):
@@ -116,4 +162,4 @@ def stake_list(wallet_name: str = "stake_2"):
 
 if __name__ == "__main__":
     unlock_wallets()
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=9000)
