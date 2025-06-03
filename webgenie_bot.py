293d74d222b8b505538a693096d5e864b764c2aa
larry-neil-1206 <larryneilmcgowen1206@gmail.com>
2025-06-03 12:02:19 -0500

    feat: integrate discord and twitter bot

--- webgenie_bot.py
+++ webgenie_bot.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from twitter_bot.twitter_bot_x import TwitterBotX
 
 
 WEBHOOK_URL = "https://discord.com/api/webhooks/1379490127151370260/M3gi6L-7pyzl6EoTfc6vUj6SayRyY7tCT0BD2Bv5DK0sm7SUKxdseKzV2GZOjXJkR4CX"
@@ -18,8 +19,14 @@ def send_message(content):
     else:
         print(f"Failed to send message: {response.status_code}, {response.text}")
 
+def callback(username, content):
+    print(f"Username: {username}")
+    print(f"Content: {content}")
+    send_message(f"""New tweet from {username}: {content}""")
+
 def main():
-    send_message("Hello from the webhook!")
+    bot = TwitterBotX()
+    bot.check_new_tweets(["OpenGradient"], callback)
 
 if __name__ == "__main__":
     main()
