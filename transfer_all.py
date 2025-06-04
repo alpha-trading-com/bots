1aeacc1e65ae6a31a48a4de7cded13e6379d3a1d
larry-neil-1206 <larryneilmcgowen1206@gmail.com>
2025-06-04 08:56:45 -0500

    chore: update handles

--- transfer_all.py
+++ transfer_all.py
@@ -2,7 +2,7 @@ import time
 import bittensor as bt
 from utils.const import sn_vali_addr
 from utils.utils import *
-
+from constants import ROUND_TABLE_HOTKEY
         
 if __name__ == '__main__':
     
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     wallet = bt.wallet(name=wallet_name)
     wallet.unlock_coldkey()
     #pool_address = '5H1hpYcpLgCYLthKwEnmWzjWMVxvmrv1UckZRxfVFHZkkZzv'
-    dest_hotkey = "5FKz1PAcB1y5vn3aGqNog2vyxagrKMjbkMRpdjY9cuXAG6pD"
+    dest_hotkey = ROUND_TABLE_HOTKEY
 
     print("Press Ctrl+C to stop the script")
     
@@ -53,24 +53,32 @@ if __name__ == '__main__':
                     subtensor.wait_for_block()
             
             deregged_hotkeys = [
-                '5HYpPntNoXLFhp7GedCssWXTTS95Pksxxic8TkUPvLwnpPtR',
-                '5D2MwaGVq8Zm6UsnmLGM8yg4tybzbwUtjsfNLJfDdXrRbNq1',
-                '5DLrM2hpH6v1qu3GHMFFTD1Vh3jRCJiEeG6WxQD61nvMzxQe',
-                '5FqXgm3se8BbXnZrD1eRTng5yJbvbZ7EAif34c3Dr2BMb3bV',
-                '5D1uVctXmAKwippo4fxssKRhypE6UDG26cwXguHDqxCBEu97',
-                '5ECW6RuP491ZMehBLg58KyhQL17Srz5yrqBR75BVD5dYV25K',
-                '5FQo1QKJWccAD9UbuACbaR3odkBubdMkaeny4Wfwnmq1bP8v',
-                '5CAyb5bHy7X22sXwPXVDATErYYjTgEbmaGv5YcxL9nYPZUmb',
-                '5D5ZErASWQcbRSPi3pNhEkaDneAcT2A2gFMAzmrt7kCpEGSp',
-                '5Eyhn77YxyMwdELoeK5ts9nM298YXYyZ3sWU3hZXBhFkG1q6',
-                '5H63SQzG5B1oHz4mRkzBVJXmRSb8bmJGu7KeeUybzH7sHhjh',
-                '5HgWcERinNgH8nV2VB22rt6mKsdhT4atzgs7sbxBoHZacZ4n',
-                '5HH89vMvEHpgdo7ET55iiE7PsCvyrXxyYXkDSAWmmYDndxtP',
-                '5FCxQ1kv4q3tgiTMUH7845wDsGho9oGSrmKevpYuRDnWgpgD',
-                '5HC7FMzgpy529kRP9LP2VDwCbJmDQQKRyfUkUDbgjLL3vHkN',
-                '5GGymrP2X64ko3nxLEP4iR3g8rgrQQzeiL4e6VNWiKXgcciZ',
-                '5EveXqcAc85GzXWiXnmnDstd4Hx5Kpo8Hw58BPGQqpU7dFnq',
-                '5GxHLxZDntjf8dbwJEKMXR2NvGckoJUapc7Gsj3bBbA96nf9',
+                "5HYpPntNoXLFhp7GedCssWXTTS95Pksxxic8TkUPvLwnpPtR",
+                "5HQSAGSWAaPZZ6Yxxgg3GQm5Dm6EzPjPd7BtFpkWBwq4ToL1",
+                "5C7ZKkwDTSf89pKGTbD8BjqvwyaWyN8qZY38wWQxybQMFcjp",
+                "5DXD5qbizv5Nct44VwrEYvRvzLRNNjurw2XgM4DtR2kC1FLF",
+                "5D2MwaGVq8Zm6UsnmLGM8yg4tybzbwUtjsfNLJfDdXrRbNq1",
+                "5FWq3nMw1MPDx2i4n4bKd4bugjmLPLtYsbBKnU73VeCstCdx",
+                "5G6MYC2iZdqA6atwjVAgw3nyjvmXygJ4GEib9qox2MfWANWc",
+                "5CyZQs1xHzLuvutoeAkh486faH78cZtE3hmiukAcgtMTHsdD",
+                "5DLrM2hpH6v1qu3GHMFFTD1Vh3jRCJiEeG6WxQD61nvMzxQe",
+                "5EPcWMLcGkcpAsGpiGvFnTZMWvVpZs6dNcDGiKqxWb6ZCaSR",
+                "5EFWKHmtKc2brG4mvywT2PpbZmVskcMHXHZtjR8A5s4Lftmd",
+                "5EedHwQSYZLAtUUmQk6R2d8Hyh3Msn5ZzVdQCJWWU6ZcZXTB",
+                "5GgPowZon5VivBa7Gu9LVjYUAtQS7ngxSbGboADDrBuw6uuC",
+                "5HDtQiPjdCPhM8zh5Jv6bfKLJNu3mESXJBGaebEzYF3NRmyu",
+                "5HC7FMzgpy529kRP9LP2VDwCbJmDQQKRyfUkUDbgjLL3vHkN",
+                "5GGymrP2X64ko3nxLEP4iR3g8rgrQQzeiL4e6VNWiKXgcciZ",
+                "5EveXqcAc85GzXWiXnmnDstd4Hx5Kpo8Hw58BPGQqpU7dFnq",
+                "5FCxQ1kv4q3tgiTMUH7845wDsGho9oGSrmKevpYuRDnWgpgD",
+                "5D5ZErASWQcbRSPi3pNhEkaDneAcT2A2gFMAzmrt7kCpEGSp",
+                "5HgWcERinNgH8nV2VB22rt6mKsdhT4atzgs7sbxBoHZacZ4n",
+                "5D1uVctXmAKwippo4fxssKRhypE6UDG26cwXguHDqxCBEu97",
+                "5FQo1QKJWccAD9UbuACbaR3odkBubdMkaeny4Wfwnmq1bP8v",
+                "5CAyb5bHy7X22sXwPXVDATErYYjTgEbmaGv5YcxL9nYPZUmb",
+                "5EZ2NNBAjEkcuoDaxoH7N59YwTWZiHy6JiuCkMZHG3WDEKQv",
+                "5EhM2GSQXUPV9Je8X4NKz2VGUhV2kzNDPcfb1qfF4PYvzNuE",
+                "5ECW6RuP491ZMehBLg58KyhQL17Srz5yrqBR75BVD5dYV25K",
             ]
             for hotkey_addr in deregged_hotkeys:
                 stake = subtensor.get_stake(
