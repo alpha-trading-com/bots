768b8fb2a19a39eec2fb660468d652c204666c48
larry-neil-1206 <larryneilmcgowen1206@gmail.com>
2025-09-16 08:14:14 -0500

    fix: price

--- watch_pool.py
+++ watch_pool.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
             tao_flow = [float(now_tao_in[i] - prev_tao_in[i]) for i in range(len(now_tao_in))]
             for i in range(len(tao_flow)):
                 if abs(tao_flow[i]) >= threshold:
-                    print(f"SN {i:2d} => {round(float(subnet_infos[i].price), 5):>8.5f}, {round(tao_flow[i], 2):>8.2f}")
+                    print(f"SN {i:2d} => {round(float(now_subnet_infos[i].price), 5):>8.5f}, {round(tao_flow[i], 2):>8.2f}")
 
             print("***")
             prev_tao_in = now_tao_in
