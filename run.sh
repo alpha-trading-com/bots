5c3499941654685d6a26aae5a062066d8a44cda6
pycorn0729 <hayesdominique0729@gmail.com>
2025-12-19 19:23:08 +0100

    chore: add running script

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+pm2 start  coldkey_swap_mempool.py --interpreter python3  --name "coldkey_swap_mempool" --interpreter-args "-u"
+
 pm2 start  coldkey_swap_v2.py --interpreter python3  --name "coldkey_swap_v3" --interpreter-args "-u"
 pm2 start  owner_coldkey_events.py --interpreter python3  --name "owner_coldkey_events" --interpreter-args "-u"
 pm2 start  channel_monitor_bot.py --interpreter python3  --name "channel_monitor_bot" --interpreter-args "-u"
