a994361f94afc9444d948b8e0f1adfd8148f29ee
pycorn0729 <hayesdominique0729@gmail.com>
2026-01-20 16:55:58 +0100

    chore: fix bugs in owner indexing

--- coldkey_swap_v2.py
+++ coldkey_swap_v2.py
@@ -70,13 +70,14 @@ class ColdkeySwapFetcher:
                 
                 # Get the new coldkey from call_args
                 address = ex.value.get('address', None)
-                subnet_id = owner_coldkeys.index(address)
                 # To get the old identity, use the current subnet identity from subnet_infos[subnet_id].
                 # To get the new identity, get from call_args['subnet_name'].
                 try:
+                    subnet_id = owner_coldkeys.index(address)
                     old_identity = subnet_infos[subnet_id].subnet_name
                     call_args = call.get('call_args', [])
                     new_identity = next((a['value'] for a in call_args if a['name'] == 'subnet_name'), None)
+                    
                     event_info = {
                         'event_type': IDENTITY_CHANGE_EVENT_TYPE,
                         'subnet': subnet_id,
