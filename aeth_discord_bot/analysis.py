43224fdd0dcbce632bb73587cca317af48710c85
pycorn0729 <hayesdominique0729@gmail.com>
2026-02-03 15:32:26 +0100

    chore: add jeeters info url

--- aeth_discord_bot/analysis.py
+++ aeth_discord_bot/analysis.py
@@ -6,9 +6,10 @@ import bittensor as bt
 import requests
 import re
 
-from modules.constants import GOOGLE_DOC_ID_BOTS
+from modules.constants import GOOGLE_DOC_ID_BOTS, GOOGLE_DOC_ID_JEETERS
 
 bots = []
+jeeters = {}
 subtensor = bt.Subtensor("finney")
 
 
@@ -23,7 +24,26 @@ def load_bots_from_gdoc():
     except Exception as e:
         print(f"Failed to load bots from Google Doc: {e}")
 
+def load_jeeters_from_gdoc():
+    url = f"https://docs.google.com/document/d/{GOOGLE_DOC_ID_JEETERS}/export?format=txt"
+    try:
+        global jeeters
+        
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        text = response.text
+        # Each pair is like: <wallet_address> <owner_name>
+        # build a dict mapping wallet address to owner name
+        pattern = r'(5[1-9A-HJ-NP-Za-km-z]{47})\s+([^\s]+)'
+        for match in re.findall(pattern, text):
+            address, owner = match
+            jeeters[address] = owner
+
+    except Exception as e:  
+        print(f"Failed to load jeeters from Google Doc: {e}")
+
 load_bots_from_gdoc()
+load_jeeters_from_gdoc()
 
 def get_bot_staked_in_subnet(subnet_id: int) -> tuple[float, list[dict]]:
     total_staked_amount = 0.0
@@ -47,7 +67,31 @@ def get_bot_staked_in_subnet(subnet_id: int) -> tuple[float, list[dict]]:
                 "staked_amount": bot_staked_amount,
             })
     return total_staked_amount, bot_staked_infos
-        
+
+
+def get_jeeter_staked_in_subnet(subnet_id: int) -> tuple[float, list[dict]]:
+    total_staked_amount = 0.0
+    subnet = subtensor.subnet(netuid=subnet_id)
+    batch_size = 60
+    jeeter_staked_infos = []
+    for i in range(0, len(jeeters), batch_size):
+        batch = jeeters[i:i+batch_size]
+        stake_infos = subtensor.get_stake_info_for_coldkeys(coldkey_ss58s=batch)
+        for jeeter, stake_info in stake_infos.items():
+            jeeter_staked_amount = 0.0
+            for stake in stake_info:
+                if stake.netuid != subnet_id:
+                    continue
+                jeeter_staked_amount += stake.stake.tao * subnet.price.tao
+            total_staked_amount += jeeter_staked_amount
+            if jeeter_staked_amount < 0.5:
+                continue
+            jeeter_staked_infos.append({
+                "jeeter": jeeter,
+                "staked_amount": jeeter_staked_amount,
+            })
+    return total_staked_amount, jeeter_staked_infos
+
 def get_subnet_info(subnet_id: int) -> dict:
     subnet: bt.DynamicInfo = subtensor.subnet(netuid=subnet_id)
     print(subnet)
