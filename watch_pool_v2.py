f31c83e2bf818d5b58d65a398f562db8be98916a
pycorn0729 <hayesdominique0729@gmail.com>
2026-02-04 14:31:00 +0100

    chore: add new jeeter

--- watch_pool_v2.py
+++ watch_pool_v2.py
@@ -63,6 +63,7 @@ def load_wallet_owners_from_gdoc():
     wallet_owners["5FLQ2m1ZgVd2qXfE4ZXtxyuqmjjJHycKqFEWvExCiNzUtEEe"] = "Jeeter"
     wallet_owners["5FnWKpesLZj1ZknKJZ6bzF3VucRxgD7VE4MFVDkh3WDbeUbL"] = "Jeeter"
     wallet_owners["5HkGCkce7aKxinYtU588kjt7sy2HKrKgKyhbNoe13kvrPFT2"] = "Jeeter"
+    wallet_owners["5Ge57pPLDUKApt49W9ZFDsrjEMVMD1Xsn1euc8EJVHECU6Jn"] = "Jeeter"
 
         
 def refresh_bots_periodically(interval_minutes=REFRESH_INTERVAL):
